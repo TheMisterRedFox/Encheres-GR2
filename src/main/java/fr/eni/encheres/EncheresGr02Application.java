35fed8e1955e90174783895244a313f6ab7b098a
Romain CORNUAULT <rougefox85@gmail.com>
2025-02-24 14:23:53 +0100

    initial commit

---
+++ src/main/java/fr/eni/encheres/EncheresGr02Application.java
@@ -0,0 +1,13 @@
+package fr.eni.encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncheresGr02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncheresGr02Application.class, args);
+	}
+
+}
