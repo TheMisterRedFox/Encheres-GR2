ec411574b6244bf1946cc57ebe0b6d43cea6aff9
Lucas Guerin <lucas.guerin.pro31@gmail.com>
2025-02-25 11:15:54 +0100

    #15 add enchere controller

--- src/main/java/fr/eni/encheres/controllers/EnchereController.java
+++ src/main/java/fr/eni/encheres/controllers/EnchereController.java
@@ -1,5 +1,15 @@
 package fr.eni.encheres.controllers;
 
-public class EnchereController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/encheres")
+public class EnchereController {
+	
+	@GetMapping
+	private String afficherEncheres() {
+		return "index";
+	}
 }
