3aba12899257c4fe77c66a95072d6241ad4be18a
Lucas Guerin <lucas.guerin.pro31@gmail.com>
2025-02-25 10:40:20 +0100

    #19 création de la bo

---
+++ src/main/java/fr/eni/encheres/bo/ArticleVendu.java
@@ -0,0 +1,146 @@
+package fr.eni.encheres.bo;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class ArticleVendu {
+	
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDateTime dateDebutEncheres;
+	private LocalDateTime dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	private String etatVente;
+
+	private Utilisateur vendeur;
+	private Retrait retrait;
+	private Categorie categorie;
+	
+	public ArticleVendu() {}
+	
+	public ArticleVendu(int noArticle, String nomArticle, String description, LocalDateTime dateDebutEncheres,
+			LocalDateTime dateFinEncheres, int miseAPrix, int prixVente, String etatVente, Utilisateur vendeur,
+			Retrait retrait, Categorie categorie) {
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+		this.vendeur = vendeur;
+		this.retrait = retrait;
+		this.categorie = categorie;
+	}
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+	public void setDateDebutEncheres(LocalDateTime dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+	public LocalDateTime getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+	public void setDateFinEncheres(LocalDateTime dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+	public int getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public String getEtatVente() {
+		return etatVente;
+	}
+
+	public void setEtatVente(String etatVente) {
+		this.etatVente = etatVente;
+	}
+
+	public Utilisateur getVendeur() {
+		return vendeur;
+	}
+
+	public void setVendeur(Utilisateur vendeur) {
+		this.vendeur = vendeur;
+	}
+
+	public Retrait getRetrait() {
+		return retrait;
+	}
+
+	public void setRetrait(Retrait retrait) {
+		this.retrait = retrait;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(noArticle);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArticleVendu other = (ArticleVendu) obj;
+		return noArticle == other.noArticle;
+	}
+	
+	
+	
+}
--- src/main/java/fr/eni/encheres/bo/Enchere.java
+++ src/main/java/fr/eni/encheres/bo/Enchere.java
@@ -1,5 +1,56 @@
 package fr.eni.encheres.bo;
 
-public class Enchere {
+import java.time.LocalDateTime;
 
+public class Enchere {
+	
+	private LocalDateTime dateEnchere;
+	private int montantEnchere;
+	
+	private ArticleVendu article;
+	private Utilisateur encherisseur;
+	
+	private Enchere() {}
+
+	public Enchere(LocalDateTime dateEnchere, int montantEnchere, ArticleVendu article, Utilisateur encherisseur) {
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+		this.article = article;
+		this.encherisseur = encherisseur;
+	}
+
+	public LocalDateTime getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(LocalDateTime dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+	public ArticleVendu getArticle() {
+		return article;
+	}
+
+	public void setArticle(ArticleVendu article) {
+		this.article = article;
+	}
+
+	public Utilisateur getEncherisseur() {
+		return encherisseur;
+	}
+
+	public void setEncherisseur(Utilisateur encherisseur) {
+		this.encherisseur = encherisseur;
+	}
+
+	
+	
 }
