81bb8818affa880b514f1c0f5ee590ce6bc40222
Lucas Guerin <lucas.guerin.pro31@gmail.com>
2025-02-25 10:05:24 +0100

    add modele crud

---
+++ src/main/java/fr/eni/encheres/dal/ICrudRepository.java
@@ -0,0 +1,16 @@
+package fr.eni.encheres.dal;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICrudRepository<T> {
+	 
+	 List<T> findAll();
+	 
+	 Optional<T> findById(int id);
+	 
+	 void save(T t);
+	 
+	 void delete(int id);
+}
+	 
