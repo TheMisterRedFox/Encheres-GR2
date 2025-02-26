4a2db9e30627c353233d69da23785ea0b4c68366
Lucas Guerin <lucas.guerin.pro31@gmail.com>
2025-02-26 16:27:37 +0100

    #35 ajout d'une vue pour les details d'un article

--- src/main/java/fr/eni/encheres/controllers/ArticleController.java
+++ src/main/java/fr/eni/encheres/controllers/ArticleController.java
@@ -5,11 +5,7 @@ import org.springframework.ui.Model;
 import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Categorie;
@@ -24,19 +20,35 @@ public class ArticleController {
 
 	private final ArticleService articleService;
 	private final CategorieService categorieService;
-	
+
+	// Controlleur
 	public ArticleController(ArticleService articleService, CategorieService categorieService) {
 		this.articleService = articleService;
 		this.categorieService = categorieService;
 	}
 
+	// Affiche le formulaire de création d'un article
 	@GetMapping("/ajouter")
 	private String afficherFormArticle(Model model) {
 		model.addAttribute("article", new ArticleVendu());
 		model.addAttribute("categories", categorieService.findAll());
 		return "pages/articles/formulaire-articles";
 	}
-	
+
+	// Affiche les details d'une vente
+	@GetMapping("/{noArticle}")
+	public String getVenteDetails(@PathVariable("noArticle") int noArticle, Model model) {
+
+		Optional<ArticleVendu> optArticle = articleService.findById(noArticle);
+
+		if(optArticle.isPresent()) {
+			model.addAttribute("article", optArticle.get());
+			return "pages/ventes/details-vente";
+		}
+		return "redirect:/articles/";
+	}
+
+	// Gère la redirection après un enregistrement
 	@PostMapping("/enregistrer")
 	private String ajouterArticle(@ModelAttribute ArticleVendu article, 
 			@RequestParam("category") int noCategorie,
@@ -44,11 +56,11 @@ public class ArticleController {
 			@RequestParam ("codePostal") String codePostal,
 			@RequestParam ("ville") String ville) {
 		Optional<Categorie> optCategorie = categorieService.findById(noCategorie);
-		
+
 		if(optCategorie.isPresent()) {
 			article.setCategorie(optCategorie.get());
 		}
-		
+
 		article.setRetrait(new Retrait(rue, codePostal, ville));
 		articleService.save(article);
 		return "redirect:/articles";
